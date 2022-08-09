66730b43e30a866a3da6c0081ecd320b1070341b
dolsen0 <derek.olsen@gmail.com>
2022-08-09 14:33:02 -0400

    created initial form

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+# React Forms
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+const [title,setTitle] = useState("Enter a title")
+const[description, setDescription] = useState("Please describe something...anything")
+const[author, setAuthor] = useState("todd")
+
+
+console.log({title})
+console.log({description})
+console.log({author})
+
+
+
+function formSubmit(e) {
+  e.preventDefault();
+  console.log("Form Submitted")
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form onSubmit={formSubmit}>
+        <h1>Comments</h1>
+
+          {/* text field single line. */}
+
+        <label>Title: </label>
+        <input 
+        type = "text" 
+        value = {title}
+        onChange={(e)=>{setTitle(e.target.value)}} 
+        />
+        <h3>Title: {title}</h3>
+
+
+
+        {/* Description field */}
+
+        <label>Description: </label>
+        <textarea value = {description}
+        onChange={(e)=>{setDescription(e.target.value)}}
+        >Hello</textarea>
+
+        <h3>Description: {description}</h3>
+
+
+
+        {/* Author Section */}
+        <label>Author</label>
+        <select value = {author}
+        onChange={(e) => {setAuthor(e.target.value)}}
+
         >
-          Learn React
-        </a>
-      </header>
+
+          <option value ="">Choose one</option>
+          <option value ="todd">Todd</option>
+          <option value ="ludwigson">Ludwigson</option>
+        </select>
+
+        <h3>
+        Author: {author}
+        </h3>
+
+
+
+        <button>Submit Form</button>
+
+        </form>
     </div>
   );
 }
