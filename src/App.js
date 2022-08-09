22c1f1ed3f09007a8b0035fc8fdeb7c41d91482f
dolsen0 <derek.olsen@gmail.com>
2022-08-09 15:47:15 -0400

    added form submit capabilities

--- src/App.js
+++ src/App.js
@@ -1,73 +1,97 @@
 
+
 import { useState } from 'react';
 import './App.css';
 
 function App() {
 
-const [title,setTitle] = useState("Enter a title")
-const[description, setDescription] = useState("Please describe something...anything")
-const[author, setAuthor] = useState("todd")
+  const [formSubmitted,setFormSubmitted] = useState(false)
+  const [errorMessage,setErrorMessage] = useState("")
+  const [title,setTitle] = useState('this is the title')
+  const [description,setDescription] = 
+                              useState("your description")
+  const [author,setAuthor] = useState("todd albert")
 
+  // console.log(title)
 
-console.log({title})
-console.log({description})
-console.log({author})
+  //const formSubmit = async (e) => {
 
+  async function  formSubmit(e)  {
+    try {
+    e.preventDefault();
+    //console.log("form submitted")
 
+    // const comment = {
+    //     title:title,
+    //     description:description,
+    //     author:author,
+    // }
+    const comment = {
+      title,
+      description,
+      author,
+  }
 
-function formSubmit(e) {
-  e.preventDefault();
-  console.log("Form Submitted")
-}
+    console.log("form submitted with ",comment)
+
+    // really submit it to an api
+    const results = await fetch("https://sql.bocacode.com/comments",{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(comment)
+    })
+    console.log(results)
+    const data = await results.json()
+
+    console.log(data)
+    } catch (error) {
+      console.error(error)
+      setErrorMessage("there was an error submitting your comment" + error.toString())
+    }
+  }
+
 
 
   return (
     <div className="App">
-      <form onSubmit={formSubmit}>
-        <h1>Comments</h1>
+        <form onSubmit={formSubmit}>
+            <h1>Comments</h1>
 
-          {/* text field single line. */}
+             {/* here goes the title */}
+            <label>Title</label>
+            <input 
+            type="text" 
+            value={title} 
+            onChange={(e)=>{setTitle(e.target.value)}}
+            />
+            <h3>{title}</h3>
 
-        <label>Title: </label>
-        <input 
-        type = "text" 
-        value = {title}
-        onChange={(e)=>{setTitle(e.target.value)}} 
-        />
-        <h3>Title: {title}</h3>
+            {/* this is the description */}
+            <label>Description</label>
+            <textarea 
+            value={description}
+            onChange={(e)=>{setDescription(e.target.value)}} />
+         
+            <h3>{description}</h3>
 
+            {/* THIS IS THE AUTHOR */}
+            <label>Author</label>
+            <select value={author}
+            onChange={(e)=> {setAuthor(e.target.value)}}
+            >
+              <option value="" selected>Choose One</option>
+              <option value="todd albert">Doctor Todd</option>
+              <option value="ludwigson" >Ludwigson</option>
+              <option value="other">Other</option>
+            </select>
+            <h3>{author}</h3>
 
-
-        {/* Description field */}
-
-        <label>Description: </label>
-        <textarea value = {description}
-        onChange={(e)=>{setDescription(e.target.value)}}
-        >Hello</textarea>
-
-        <h3>Description: {description}</h3>
-
-
-
-        {/* Author Section */}
-        <label>Author</label>
-        <select value = {author}
-        onChange={(e) => {setAuthor(e.target.value)}}
-
-        >
-
-          <option value ="">Choose one</option>
-          <option value ="todd">Todd</option>
-          <option value ="ludwigson">Ludwigson</option>
-        </select>
-
-        <h3>
-        Author: {author}
-        </h3>
-
-
-
-        <button>Submit Form</button>
+            <button>Submit Form</button>
+            {errorMessage && 
+              <h1>damnit: <br /> {errorMessage}</h1>
+            }
 
         </form>
     </div>
